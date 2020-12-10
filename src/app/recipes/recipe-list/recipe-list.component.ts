673d99ba3c88e8188f47edd9df08bd325df80ceb
Julian Mikołajczyk <s18734@pjwstk.edu.pl>
2020-12-10 15:24:33 +0100

    Added viewing recipe logic

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -10,14 +10,17 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
     new Recipe("Scrambled eggs", "Scramble the eggs", "http://t3.gstatic.com/images?q=tbn:ANd9GcRNiF6rn5PxDw6NRxawYt2mSmj2SsRVLxmjCvTO6JWpGkol-h_VWxVolM_JrfMpF7n2Hw2zbwCbdFZxTmmj7xE"),
-    new Recipe("Scrambled eggs", "Scramble the eggs", "http://t3.gstatic.com/images?q=tbn:ANd9GcRNiF6rn5PxDw6NRxawYt2mSmj2SsRVLxmjCvTO6JWpGkol-h_VWxVolM_JrfMpF7n2Hw2zbwCbdFZxTmmj7xE")
+    new Recipe("Scrambled eggs22", "Scramble the eggs1", "http://t3.gstatic.com/images?q=tbn:ANd9GcRNiF6rn5PxDw6NRxawYt2mSmj2SsRVLxmjCvTO6JWpGkol-h_VWxVolM_JrfMpF7n2Hw2zbwCbdFZxTmmj7xE")
   ];
 
-
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
